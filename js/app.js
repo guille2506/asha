1fda30500be1400bd1a96bb45c5c3273ed02c079
guille2506 <inowbut456@gmail.com>
2023-09-24 13:24:51 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// // Selecciona el ícono de menú y el menú lateral
+// var menuIcon = document.getElementById("menuIcon");
+// var mySidebar = document.getElementById("mySidebar");
+
+// // Función para abrir/cerrar el menú lateral
+// function toggleMenu() {
+//   if (mySidebar.style.display === "block") {
+//     mySidebar.style.display = "none";
+//   } else {
+//     mySidebar.style.display = "block";
+//   }
+// }
+
+// // Agrega un evento de clic al ícono de menú para llamar a la función toggleMenu
+// menuIcon.addEventListener("click", toggleMenu);
